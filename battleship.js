0f0a1f5030702c42afdc2df3b7a1e1522b6ee65b
Dato <davituliani.dato@yahoo.com>
2022-01-28 13:16:09 +0300

    rus

--- battleship.js
+++ battleship.js
@@ -7,21 +7,21 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-guess = prompt("Ready, aim, fire! (enter a number 0 - 6): ");
+guess = prompt("Готов, огонь! (введи число от 0 - 6): ");
 if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
+    alert("Пожалуйста введи корректное значение!");
 } else {
     guesses = guesses + 1;
 }
 if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
-    alert("HIT!");
+    alert("ПОПАЛ!");
     } else{
-        alert("MISS");
+        alert("ПРОМАХНУЛСЯ");
     }
 if (hits == 3) {
     isSunk = true;
-    alert("You sunk my battlesheep!");
+    alert("Ты утопил мой корабль!");
 }
 }
 
