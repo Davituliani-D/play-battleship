0ccb9d57e38c691b3d7babd7df962334741626a3
Dato <davituliani.dato@yahoo.com>
2022-01-28 13:53:30 +0300

    game

--- battleship.js
+++ battleship.js
@@ -7,21 +7,14 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-<<<<<<< HEAD
 guess = prompt("Готов, огонь! (введи число от 0 - 6): ");
 if (guess < 0 || guess > 6) {
     alert("Пожалуйста введи корректное значение!");
-=======
-guess = prompt("Ready, aim, fire! (enter a number 0 - 6): ");
-if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
->>>>>>> master
 } else {
     guesses = guesses + 1;
 }
 if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
-<<<<<<< HEAD
     alert("ПОПАЛ!");
     } else{
         alert("ПРОМАХНУЛСЯ");
@@ -29,15 +22,6 @@ if (guess == location1 || guess == location2 || guess == location3) {
 if (hits == 3) {
     isSunk = true;
     alert("Ты утопил мой корабль!");
-=======
-    alert("HIT!");
-    } else{
-        alert("MISS");
-    }
-if (hits == 3) {
-    isSunk = true;
-    alert("You sunk my battlesheep!");
->>>>>>> master
 }
 }
 
